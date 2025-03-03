3ae595f627aa0c7ed138c562ae527eac67394a8e
Nathy-bajo <bajon7680@gmail.com>
2025-03-03 01:29:54 +0100

    revert unnecessary changes

--- contracts/contracts/IncheonContents.sol
+++ contracts/contracts/IncheonContents.sol
@@ -10,9 +10,6 @@ contract IncheonContents is ERC1155, Ownable {
     string private _name;
     string private _symbol;
 
-    // Mapping to track minted content per address
-    mapping(address => mapping(uint256 => bool)) private _mintedContent;
-
     constructor(address owner, string memory name_, string memory symbol_, string memory uri_) ERC1155(uri_) Ownable(owner) {
         _name = name_;
         _symbol = symbol_;
@@ -39,18 +36,10 @@ contract IncheonContents is ERC1155, Ownable {
     }
 
     function mint(address to, uint256 id, uint256 value) external onlyOwner {
-        require(to != owner(), "Creators cannot mint their own content");
-        require(!_mintedContent[to][id], "Content already minted by this address");
         _mint(to, id, value, new bytes(0));
-        _mintedContent[to][id] = true;
     }
 
     function mintBatch(address to, uint256[] memory ids, uint256[] memory values) external onlyOwner {
-        require(to != owner(), "Creators cannot mint their own content");
-        for (uint256 i = 0; i < ids.length; i++) {
-            require(!_mintedContent[to][ids[i]], "Content already minted by this address");
-            _mintedContent[to][ids[i]] = true;
-        }
         _mintBatch(to, ids, values, new bytes(0));
     }
 }
