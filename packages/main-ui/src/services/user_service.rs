4307159eb9ade078bbfb0702acf5189365f79f42
Nathy-bajo <bajon7680@gmail.com>
2025-03-10 08:11:52 +0100

    fix ci

--- packages/main-ui/src/services/user_service.rs
+++ packages/main-ui/src/services/user_service.rs
@@ -355,7 +355,7 @@ impl KaiaWallet for UserService {
             }
             UserWallet::KaiaWallet(ref wallet) => wallet.sign_transaction(tx).await,
             UserWallet::None => Err(dto::Error::WalletNotInitialized),
-            UserWallet::InternetIdentity { .. } => todo!(),
+            UserWallet::InternetIdentity { .. } => Err(dto::Error::ValidationError("Transaction signing for Internet Identity is not yet implemented.".to_string())),
         }
     }
 }
